16e8e085c905a7f5f1a5026229fbb976adce7478
Artem <kaline-son@ya.ru>
2025-09-03 16:30:52 +0700

    feat: установил grammy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.ts');
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['grammy'],
+    },
+}
 
 export default withNextIntl(nextConfig);
