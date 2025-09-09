3d94e36fc28ee681dd97cf2b914af76444784ec1
Artem <kaline-son@ya.ru>
2025-09-09 11:24:26 +0700

    feat: установил drizzle и postgres

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.ts');
 
 const nextConfig: NextConfig = {
+    reactStrictMode: true,
     experimental: {
         serverComponentsExternalPackages: ['grammy'],
     },
