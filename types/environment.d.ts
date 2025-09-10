6385cfc953b2b1e3ff392c89962f23ac32500b5b
Artem <kaline-son@ya.ru>
2025-09-10 12:03:12 +0700

    upd: типы переменных окружения

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -8,9 +8,12 @@ declare global {
             // https://nextjs.org/docs/messages/non-standard-node-env
             // не должно быть в .env файле, next.js самостоятельно проставляет значение
             NODE_ENV?: "production" | "development" | "test";
-
+            /**Токен от bot father для итога проекта */
             TELEGRAM_BOT_TOKEN?: string;
+            /**Токен от bot father для отладки проекта*/
             TELEGRAM_DEV_BOT_TOKEN?: string;
+            /**Сгенерированная ссылка для Drizzle в Supabase. Пароль в ссылке проставил из переменных окружения проекта с сайта vercel */
+            DATABASE_URL?: string;
         }
     }
 }
